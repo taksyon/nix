dd3a08822e192f9e3e0d7d736ea09c61cc5509f4
dylan <dylanmyers316@gmail.com>
2025-12-22 12:00:59 -0500

    formatting

--- 20-modules/20.2-home/20.23-apps/zathura.nix
+++ 20-modules/20.2-home/20.23-apps/zathura.nix
@@ -2,11 +2,12 @@
 {
   # zathura dark mode
   xdg.configFile."zathura/zathurarc".text = ''
-    set default-bg "#020424"
-    set default-fg "#d4d4d4"
-    set recolor "true"
-    set recolor-keep "true"
-    set recolor-lightcolor "#000000"
-    set recolor-darkcolor "#c5c5c5"
+    # syntax: sh
+      set default-bg "#020424"
+      set default-fg "#d4d4d4"
+      set recolor "true"
+      set recolor-keep "true"
+      set recolor-lightcolor "#000000"
+      set recolor-darkcolor "#c5c5c5"
   '';
 }
